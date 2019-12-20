536391376aafe98e5de28d0a3df4693734387d86
excavator <str_token@mail.ru>
2019-12-20 18:27:33 +0300

    add Edit form

--- app/assets/javascripts/components/_fruit.js.jsx
+++ app/assets/javascripts/components/_fruit.js.jsx
@@ -1,9 +1,44 @@
 class Fruit extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      editable: false
+    }
+    this.handlerEdit = this.handlerEdit.bind(this)
+  }
+
+  handlerEdit(){
+    this.setState({
+      editable: !this.state.editable
+    })
+  }
+
   render(){
+
+    let name = this.state.editable
+      ? <input 
+          type="text"
+          ref={input => this.name = input}
+          defaultValue={this.props.fruit.name}
+        />
+      : <h3>{ this.props.fruit.name }</h3>
+    
+    let description = this.state.editable
+      ? <input 
+          type="text"
+          ref={input => this.description = input}
+          defaultValue={this.props.fruit.description}
+        />
+      : <p>{ this.props.fruit.description }</p>
+    
     return(
       <div>
-        <h1>{ this.props.fruit.name }</h1>
-        <p>{ this.props.fruit.description }</p>
+        {name}
+        {description}
+        <button
+          onClick={()=> this.handlerEdit()}
+        >{ this.state.editable? 'Submit' : 'Edit'}</button>
         <button
           onClick={()=> this.props.handlerDelete(this.props.fruit.id)}
         >Delete</button>
