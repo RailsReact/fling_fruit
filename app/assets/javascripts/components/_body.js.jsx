b70eb4031a2cc0f01399bab67ea4a3f0b74460b6
excavator <str_token@mail.ru>
2019-12-20 15:27:15 +0300

    add hith level component Body

---
+++ app/assets/javascripts/components/_body.js.jsx
@@ -0,0 +1,23 @@
+class Body extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      fruits: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('/api/v1/fruits.json')
+      .then((response)=>{return response.json()})
+      .then((date)=> { this.setState({ fruits: date }) });
+  }
+
+  render(){
+    return(
+      <div>
+        <AllFruits fruits={this.state.fruits} />
+      </div>
+    )
+  }
+}
